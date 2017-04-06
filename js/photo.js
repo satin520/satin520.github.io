ecbd38927f301c92fe5c7e1d50e0d96adf5c1dd7
satin520 <244752937@qq.com>
2017-04-06 10:03:23 +0800

    Site updated: 2017-04-06 10:03:21

--- js/photo.js
+++ js/photo.js
@@ -1,56 +1,52 @@
-/**
- * Created by zlw on 2016/1/2.
- */
-define([], function () {
-    return {
-        page: 1,
-        offset: 20,
-        init: function () {
-            var that = this;
-            $.getJSON("/photo/output.json", function (data) {
-                that.render(that.page, data);
 
-                that.scroll(data);
-            });
-        },
+$(function() {
+  var page = 1;
+  var offset = 20;
+  var BoxImg = function() {
+    this.init();
+  }
+  BoxImg.prototype = {
+    init: function() {
+      var that = this;
+      $.getJSON("/photos/output.json", function(data) {
+        that.render(that.page, data);
+        that.scroll(data);
+      });
+    },
 
-        render: function (page, data) {
-            var begin = (page - 1) * this.offset;
-            var end = page * this.offset;
-            if (begin >= data.length) return;
-            var html, li = "";
-            for (var i = begin; i < end && i < data.length; i++) {
-                li += '<li><div class="img-box">' +
-                    '<a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/satin520/satin520.github.io/master/photos/' + data[i] + '?raw=true"></a>' +
-                    '<img lazy-src="https://raw.githubusercontent.com/satin520/satin520.github.io/master/photos/' + data[i] + '?raw=true" />' +
-                    '</li>';
-            }
+    render: function(page, data) {
+      var begin = (page - 1) * this.offset;
+      var end = page * this.offset;
+        console.log(page)
+      if (begin >= data.length) return;
+      var html, li = "";
+      for (var i = 0; i < data.length; i++) {
 
-            //html = '<section class="archives album">' +
-            //    '<ul class="img-box-ul">' + li + '</ul>' +
-            //    '</section>';
+        li += '<li><div class="img-box">' +
+          '<a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/satin520/satin520.github.io/master/photos/' + data[i] + '"></a>' +
+          '<img src="https://raw.githubusercontent.com/satin520/satin520.github.io/master/photos/' + data[i] + '" />' +
+          '</li>';
+      }
 
+      $(".img-box-ul").append(li);
+      $(".img-box-ul").lazyload();
+      $("a[rel=example_group]").fancybox();
+    },
 
-            $(".img-box-ul").append(li);
-            $(".img-box-ul").lazyload();
-            $("a[rel=example_group]").fancybox();
-        },
+    scroll: function(data) {
+      var that = this;
+      $(window).scroll(function() {
+        var windowPageYOffset = window.pageYOffset;
+        var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
+        var sensitivity = 0;
 
-        scroll: function (data) {
-            var that = this;
-            $(window).scroll(function() {
-                var windowPageYOffset = window.pageYOffset;
-                var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
-                var sensitivity = 0;
+        var offsetTop = $(".instagram").offset().top + $(".instagram").height();
 
-                var offsetTop = $(".instagram").offset().top + $(".instagram").height();
-
-                if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
-                    that.render(++that.page, data);
-                }
-            })
+        if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
+          that.render(++that.page, data);
         }
+      })
     }
-
+  }
+  var boximg=new BoxImg();
 })
-
